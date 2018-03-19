4585b55c36223ab0ccf585501b546e0b530643f8
mabysh <max.byshnev@protonmail.com>
2018-03-20 00:30:10 +0300

    tokio aware 'input_stream' added

---
+++ src/io/input_stream.rs
@@ -0,0 +1,68 @@
+use std::io;
+use std::io::Read;
+use std::os::unix::io::AsRawFd;
+use std::mem;
+
+use futures;
+use futures::Stream;
+
+use tokio::prelude::*;
+use tokio::reactor::PollEvented2;
+
+use super::super::reader::NonBlockingReader;
+
+use bytes::BytesMut;
+
+#[derive(Debug)]
+pub struct InputStream<R>
+where
+    R: AsRawFd + Read,
+{
+    io: PollEvented2<NonBlockingReader<R>>,
+    cap: Option<usize>,
+    buf: BytesMut,
+}
+
+pub fn input_stream<R: AsRawFd + Read>(fd: R, buf_capacity: Option<usize>) -> InputStream<R> {
+    let reader =
+        NonBlockingReader::from_fd(fd).expect("Failed to set O_NONBLOCK to the file descriptor");
+    InputStream {
+        io: PollEvented2::new(reader),
+        cap: buf_capacity,
+        buf: new_buf(buf_capacity),
+    }
+}
+
+impl<R: AsRawFd + Read> Stream for InputStream<R> {
+    type Item = BytesMut;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> futures::Poll<Option<BytesMut>, io::Error> {
+        match try_nb!(self.io.read_buf(&mut self.buf)) {
+            futures::Async::Ready(_i) => {
+                if self.io.get_ref().is_eof() {
+                    // EOF
+                    return Ok(None.into());
+                }
+                let len = self.buf.len();
+                if self.buf[len - 1] == b'\n' && len >= 1 {
+                    self.buf.truncate(len - 1);
+                    if self.buf[len - 2] == b'\r' && len >= 2 {
+                        self.buf.truncate(len - 2);
+                    }
+                }
+                return Ok(Some(mem::replace(&mut self.buf, new_buf(self.cap))).into());
+            }
+            futures::Async::NotReady => {
+                return Ok(Async::NotReady);
+            }
+        }
+    }
+}
+
+fn new_buf(size: Option<usize>) -> BytesMut {
+    match size {
+        Some(s) => BytesMut::with_capacity(s),
+        None => BytesMut::new(),
+    }
+}
---
+++ src/io/mod.rs
@@ -0,0 +1,10 @@
+/// I/O primitives that use NonBlockingReader as their input and implement [`Future`] or
+/// [`Stream`] traits. Intended to be used with [`tokio`] reactor.
+///
+///[`Future`]: https://docs.rs/futures/0.2.0-alpha/futures/prelude/trait.Future.html
+///[`Stream`]: https://docs.rs/futures/0.2.0-alpha/futures/prelude/trait.Stream.html
+///[`tokio`]: https://tokio-rs.github.io/tokio/tokio/reactor/index.html
+
+mod input_stream;
+
+pub use self::input_stream::{input_stream, InputStream};
